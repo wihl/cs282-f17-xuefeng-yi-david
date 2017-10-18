604071250850d077a5eb5774cea62a09f4549c8a
David Wihl <davidwihl@gmail.com>
2017-10-18 15:28:58 -0400

    minor fixes after moving to github and .py files

--- code/DQN/main.py
+++ code/DQN/main.py
@@ -1,7 +1,11 @@
+import sys
+sys.path.append('../utils')
+
 import PatientRecordProcessor
 import DQN as dqn
 import numpy as np
 import tensorflow as tf
+
 
 def train(model, histories, num_epoches=10):
     # shuffle histories
@@ -17,24 +21,24 @@ def train(model, histories, num_epoches=10):
 
 if __name__ == "__main__":
 
-	######## build training set ########
-	prefix = '../../Dataset/'
-	prp = PatientRecordProcessor(prefix + 'Sepsis_imp.csv', prefix + 'states_list.pkl')
+    ######## build training set ########
+    prefix = '../../data/'
+    prp = PatientRecordProcessor.PatientRecordProcessor(prefix + 'Sepsis_imp.csv', prefix + 'states_list.pkl')
 
-	histories = prp.build_training_memory()
+    histories = prp.build_training_history()
 
-	######## train ########
-	tf.reset_default_graph()
+    ######## train ########
+    tf.reset_default_graph()
 
-	MEMORY_SIZE = 1280
-	ACTION_SPACE = 25
-	FEATURES = 51
-	NUM_EPOCHES = 100
+    MEMORY_SIZE = 1280
+    ACTION_SPACE = 25
+    FEATURES = 51
+    NUM_EPOCHES = 100
 
-	## speicify output_path while tunning parameters
-	with tf.variable_scope('dueling'):
-	    dueling_DQN = dqn(
-	        num_actions=ACTION_SPACE, num_features=FEATURES, mem_size=MEMORY_SIZE,
-	        dueling=True, output_graph=True)
+    ## speicify output_path while tunning parameters
+    with tf.variable_scope('dueling'):
+        dueling_DQN = dqn.DQN(
+            num_actions=ACTION_SPACE, num_features=FEATURES, mem_size=MEMORY_SIZE,
+            dueling=True, output_graph=True)
 
-	train(dueling_DQN, histories, num_epoches=NUM_EPOCHES)
+    train(dueling_DQN, histories, num_epoches=NUM_EPOCHES)
--- code/state_cluster/cluster_sepsis_states.py
+++ code/state_cluster/cluster_sepsis_states.py
@@ -4,7 +4,9 @@ import pickle as pkl
 
 print ('data preparing ...')
 # read the src
-data = np.genfromtxt('../Dataset/Sepsis_imp.csv', dtype=float, delimiter=',', skip_header=1) 
+
+datapath = '../../data/'
+data = np.genfromtxt(datapath + 'Sepsis_imp.csv', dtype=float, delimiter=',', skip_header=1)
 # remove intervention, but include ventilation, sedation, RRT
 interventions = np.setdiff1d(np.arange(47, 57), [51,52,54,55,56]) #[52,53,55,57]
 data = np.delete(data, interventions, axis=1)
@@ -17,5 +19,5 @@ kmeans = KMeans(n_clusters=2000, random_state=0).fit(data)
 states_list = kmeans.labels_
 centers = kmeans.cluster_centers_
 
-pkl.dump(states_list, open('states_list.pkl', 'wb'))
-pkl.dump(centers, open('centers.pkl', 'wb'))
+pkl.dump(states_list, open(datapath + 'states_list.pkl', 'wb'))
+pkl.dump(centers, open(datapath + 'centers.pkl', 'wb'))

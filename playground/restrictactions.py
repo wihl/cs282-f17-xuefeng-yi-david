20c973163cde7c2de4e536f0bb32ee442d21af54
David Wihl <davidwihl@gmail.com>
2017-11-07 10:13:11 -0500

    Restrict Actions class and Jupyter sample

---
+++ playground/restrictactions.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+
+
+class RestrictActions(object):
+    """
+    Class RestrictActions. Given a series of episodes consisting of
+        (state, action, sprime), build a list of viable actions
+        for a given state. Viable is defined as the list of actions
+        that have been previously attempted sorted in descending order.
+        If only a single action is viable, return a single element list.
+    """
+    def __init__(self,n_states,n_actions):
+        self.n_states = n_states
+        self.n_actions = n_actions
+
+    def load_sas_as_episodes(self, episodes):
+        """
+        episodes is expected to a pandas dataframe that has already been
+        sorted by id, and then bloc (or timestamp). It is expected to
+        have at least two columns: state and action.
+        """
+        self.sas_count = np.zeros((self.n_states, self.n_actions, self.n_states))
+        i =0
+        for name, transitions in episodes:
+            state = transitions['state'].tolist()
+            action = transitions['action'].tolist()
+            for i in range(len(state)-2):
+                self.sas_count[state[i],action[i],state[i+1]] += 1
+        self.build_action_set()
+
+    def load_sas_as_array(self, array):
+        self.sas_count = np.zeros((self.n_states, self.n_actions, self.n_states))
+        for i in range(len(array)):
+            state, action, sprime = array[i]
+            self.sas_count[state,action,sprime] += 1
+        self.build_action_set()
+
+
+    def build_action_set(self):
+        """
+        Assuming the sas_count has been populated, create the list of
+        actions for a given state, sorted by frequency
+        """
+        self.action_freq = np.zeros((self.n_states,self.n_actions))
+
+        for (s,a,sprime), value in np.ndenumerate(self.sas_count):
+            if value > 0:
+                self.action_freq[int(s)][a] += value
+
+
+    def get_actions(self, state):
+        """
+        For a given state, return the list of appropriate actions sorted
+        in descending order by most often seen in the observations
+        """
+        zeros = np.where(self.action_freq[state] == 0)[0].tolist()
+        sorted_actions = np.argsort(self.action_freq[state])[::-1].tolist()
+        # Remove actions with a zero count
+        for i in zeros:
+            sorted_actions.remove(i)
+        return sorted_actions
+
+    def get_actions_per_state(self, state):
+        """
+        For a given state, return the list of number of times each action
+        was taken, ordered by action.
+        """
+        return self.action_freq[state].tolist()
